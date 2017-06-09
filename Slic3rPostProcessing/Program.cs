40cc16a99f77d6c9f642915e7f03b4ad0b05007b
Gilbert Fürer <gilbert.fuerer@feintool.com>
2017-06-09 15:14:38 +0200

    Better!

--- Slic3rPostProcessing/Program.cs
+++ Slic3rPostProcessing/Program.cs
@@ -18,6 +18,8 @@ namespace Slic3rPostProcessing
 		public static short insertedSoftSupportSegment { get; set; }
 		public static short insertedPerimeterSegment { get; set; }
 
+		public static string strTimeformat = "HHmmss-yyyyMMdd";
+
 		/// <summary>
 		/// Post processing for Slic3r to color the toolpaths to view in Craftware
 		/// </summary>
@@ -31,21 +33,37 @@ namespace Slic3rPostProcessing
 			bool show_help = false;
 
 			int verbosity = 3;
-			int repprogress = 5;
+			double repprogress = 5d;
 			string strINputFile = null;
 			string strOUTputFile = null;
+
+			bool booTimestamp = false;
+			bool boofTimestamp = true;
 
 			var p = new OptionSet() {
 				{ "i|input=", "The {INPUTFILE} to process.",
 					v => strINputFile=v },
-				{ "o|output=", "The {OUTPUTFILE} to copy the processed content to (optional).",
+
+				{ "o|output=", "The {OUTPUTFILE} filename. \n Optional. {INPUTFILE} will get overwritten if {OUTPUTFILE} is not specified.",
 					v => strOUTputFile=v },
+
+				{ "r|repprog=", "Report progress ever {PROG} percentage. Default "+ repprogress +".",
+					(double iprog) => repprogress=(iprog >= 1 & iprog<=100 ? iprog : repprogress ) },
+
+				{ "t|timestamp=","Adds a timestamp to the END of the filename.\n {+ or -}; Default = -",
+					t => booTimestamp = t != null },
+
+				{ "p|pretimestamp=","Adds a timestamp to the FRONT of the filename.\n {+ or -}; Default = + \n If both timestamps are set to true, then it will only be added to the front.",
+					t => boofTimestamp = t != null },
+
+				{ "f|formatstamp=","{FORMAT} of the timestamp. \n Default: " + strTimeformat,
+					tf => strTimeformat = tf },
+
+				{ "v|verbosity=", "Debug message verbosity. Default: "+ verbosity +". \n {INT}:\n 0 = Off \n 1 = Error \n 2 = Warning \n 3 = Info \n 4 = Verbose (will output EVER line of GCode! There will be LOTS of output!)",
+					(int v) => { if ( v >= 0 & v <5) verbosity = v; } },
+
 				{ "h|help",  "Show this message and exit. Nothing will be done.",
 					v => show_help = v != null },
-				{ "v|verbosity=", "Debug message verbosity. Default: 3. \n 0 = Off \n 1 = Error \n 2 = Warning \n 3 = Info \n 4 = Verbose (will output EVER line of GCode! There will be LOTS of output!)",
-					(int v) => { if ( v >= 0 & v <5) verbosity = v; } },
-				{ "p|progress=", "Report progress ever {PROGRESS} percentage (integer). Default 5.",
-					(int iprog) => repprogress=(iprog >= 1 & iprog<=100 ? iprog : repprogress ) }   ,
 			};
 
 			List<string> extra;
@@ -132,7 +150,7 @@ namespace Slic3rPostProcessing
 				bool StartPoint = false;
 				bool FirstLine = false;
 				string FirstLayer = null;
-				int repprogint = cLines * repprogress / 100;
+				int repprogint = (int)(cLines * repprogress / 100);
 				int repnewprog = repprogint;
 
 				int q = -1;
@@ -324,16 +342,43 @@ namespace Slic3rPostProcessing
 				{
 					System.IO.File.WriteAllText(newfilename, sb.ToString());
 
+					if (boofTimestamp & booTimestamp)
+					{
+						booTimestamp = false;
+					}
+
 					if (strOUTputFile != null & File.Exists(newfilename))
 					{
 						File.Delete(strOUTputFile);
-						File.Move(newfilename, strOUTputFile);
+						if (booTimestamp)
+						{
+							File.Move(newfilename, strOUTputFile.AppendTimeStamp());
+						}
+						else if (boofTimestamp)
+						{
+							File.Move(newfilename, strOUTputFile.PrependTimeStamp());
+						}
+						else
+						{
+							File.Move(newfilename, strOUTputFile);
+						}
 					}
 
 					if (strOUTputFile == null & File.Exists(newfilename))
 					{
 						File.Delete(strINputFile);
-						File.Move(newfilename, strINputFile);
+						if (booTimestamp)
+						{
+							File.Move(newfilename, strINputFile.AppendTimeStamp());
+						}
+						else if (boofTimestamp)
+						{
+							File.Move(newfilename, strINputFile.PrependTimeStamp());
+						}
+						else
+						{
+							File.Move(newfilename, strINputFile);
+						}
 					}
 
 					Logger.LogInfo("All done - Thank you. Will close now ... ");
@@ -374,11 +419,11 @@ namespace Slic3rPostProcessing
 		{
 			Console.WriteLine();
 			Console.WriteLine("This program is for use with Slic3r or standalone.");
-			Console.WriteLine("Slic3r - Print Settings");
-			Console.WriteLine("        -> Output options");
-			Console.WriteLine("        -> Enable Verbose G-Code (important!)");
-			Console.WriteLine("        -> Put full filename to exe and ` --i=` in Post-Processing Scripts.");
-			Console.WriteLine("        Example: c:\\temp\\Slic3rPostProcessing --i=");
+			Console.WriteLine();
+			Console.WriteLine("Slic3r  -> Print Settings -> Output options");
+			Console.WriteLine("        * Enable Verbose G-Code (!)");
+			Console.WriteLine("        * Put full filename to " + Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location) + " in Post-Processing Scripts.");
+			Console.WriteLine("        Current filename: \"" + System.Reflection.Assembly.GetEntryAssembly().Location + "\"");
 
 			Console.WriteLine();
 			Console.WriteLine("Standalone usage: Slic3rPostProcessing [OPTIONS]");
@@ -426,11 +471,32 @@ namespace Slic3rPostProcessing
 		public const int SkirtSegment = 4;
 	}
 
+	public static class MyExtensions
+	{
+		public static string AppendTimeStamp(this string fileName)
+		{
+			return Path.Combine(Path.GetDirectoryName(fileName),
+				Path.GetFileNameWithoutExtension(fileName)
+				+ "_"
+				+ DateTime.Now.ToString(Program.strTimeformat)
+				+ Path.GetExtension(fileName));
+		}
+
+		public static string PrependTimeStamp(this string fileName)
+		{
+			return Path.Combine(Path.GetDirectoryName(fileName),
+				DateTime.Now.ToString(Program.strTimeformat)
+				+ "_"
+				+ Path.GetFileNameWithoutExtension(fileName)
+				+ Path.GetExtension(fileName));
+		}
+	}
+
 	internal class Logger
 	{
 		public static void LogInfo(string message)
 		{
-			Trace.WriteLineIf(Logger.traceSwitch.TraceInfo, "INFO : " + message);
+			Trace.WriteLineIf(Logger.traceSwitch.TraceInfo, "Info : " + message);
 		}
 
 		public static void Log(string message)
@@ -455,7 +521,7 @@ namespace Slic3rPostProcessing
 
 		public static void LogVerbose(string message)
 		{
-			Trace.WriteLineIf(Logger.traceSwitch.TraceVerbose, "VERBOSE : " + message);
+			Trace.WriteLineIf(Logger.traceSwitch.TraceVerbose, "Verbose : " + message);
 		}
 
 		public static TraceSwitch traceSwitch = new TraceSwitch("Application", "Application");
